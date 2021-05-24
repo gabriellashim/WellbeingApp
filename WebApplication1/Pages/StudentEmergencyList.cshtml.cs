ab35ff33f8eb133269610e711f61e89892c328e1
Gabriella <gabriella.shim@gmail.com>
2021-05-24 14:42:07 +0930

    Added - Student emergency contact list page, Copyright free emojis, Leaders weekly status page | WIP - local databse initialising, weekly status, notification

---
+++ WebApplication1/Pages/StudentEmergencyList.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Quokka_App.Pages
+{
+    public class StudentEmergencyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
