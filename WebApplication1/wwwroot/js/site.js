1b450cfdffd2191ecefb81d08df6ea55aecef172
Gabriella <gabriella.shim@gmail.com>
2021-05-28 13:42:29 +0930

    Leaders List page- senior leader view added - allow them to change leaders. CSS file comments added.

--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -1,8 +1,7 @@
 // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-
+// function ShowAndHideDiv() applied to StudentRequest.cshtml
 function ShowAndHideDiv() {
     if (document.getElementById('Stu_Yes').checked) {
         document.getElementById('StuWhenHide').style.visibility = 'visible';
